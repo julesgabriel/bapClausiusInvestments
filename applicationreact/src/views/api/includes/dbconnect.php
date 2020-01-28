cd00033a7ceaa13202f52ad77ab504dd9961f4e7
julesgabriel <49538830+julesgabriel@users.noreply.github.com>
2020-01-28 15:20:33 +0100

    modification des mots de passe

--- applicationreact/src/views/api/includes/dbconnect.php
+++ applicationreact/src/views/api/includes/dbconnect.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-    $db = new PDO('mysql:host=localhost;port=3306;dbname=clausius;charset=utf8', 'root', 'root');
+    $db = new PDO('mysql:host=localhost;port=3306;dbname=clausius;charset=utf8', 'root', '');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 } catch (PDOException $e) {
